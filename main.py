669c7b03f5f28a7ad0e4ca1fdda59c93337411a1
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-04-18 23:05:48 +0200

    [UPDATE]

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000) # specify port as 8000
