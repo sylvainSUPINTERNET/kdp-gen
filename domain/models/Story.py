1912b22adeb074811ef8ef459cc9a7daa1e43cc1
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-04-20 00:48:44 +0200

    [WIP]

--- domain/models/Story.py
+++ domain/models/Story.py
@@ -1,18 +1,76 @@
 
 
 from infrastructure.OpenAiConfiguration import chat_GPT_complete
+import datetime
+import time
+from typing import List
 
 
 class Story():
-    story_paragraphs = []
-    backtrace_generation = ""
-    continue_text = "Continue with story"
+    story_paragraphs:List[str] = []
+    limit_req_free_plan_per_minute:int = 3
+    delay_free_plan:int = 60
     
-    def __init__(self, theme:str , itteration:int = 5):
-        self.theme = theme
-        self.itteration = itteration
+    def __init__(self, prologue:str, plan:str="free"):
         
-    def add_paragraph(self, paragraph:str):
-        chat_GPT_complete()
+        self.plan = plan
+        
+        if plan == "free":
+            print(f"Using free plan, chatgpt-3.5 turbo is limited {self.limit_req_free_plan_per_minute} requests per minute")
+        
+        
+        self.prologue = prologue
+        
+    def add_paragraph_step(self, metadata:str = None, role:str="user"):
+        print(f"Generating paragraph ({role}) -> {metadata}")
+        
+        if len(self.story_paragraphs) == 0:
+            
+            paragraph:str = chat_GPT_complete(self.prologue, "user")
+            self.story_paragraphs.append(paragraph)
+            return self
+        
+        if self.plan == "free":
+            if (len(self.story_paragraphs) % self.limit_req_free_plan_per_minute) == 0:
+                print("Limit of free plan reached waiting for 1 minute for next generation...")
+                time.sleep(self.delay_free_plan)
+                return self
+
+        if metadata is not None:
+            prologue:str = f"{metadata} {self.story_paragraphs[-1]}"
+        else:
+            prologue:str = f"Continue this story {self.story_paragraphs[-1]}"
+            
+        paragraph:str = chat_GPT_complete(prologue, role)
+        
         self.story_paragraphs.append(paragraph)
         
+        return self
+
+    
+    def get_story(self)->List[str]:
+        """ Get the story
+
+        Returns:
+            list[str]: story as array of string ( contains /r /n and so on, so trim is required )
+        """
+        
+        self.generate_epub()
+        
+        return self.story_paragraphs
+    
+    
+    
+    def generate_image(self, type:str="illustration"):
+        """TODO : Generate Image for illustration and cover
+
+        Args:
+            type (str, optional): type of image to generate for the story. Defaults to "illustration".
+        """
+        pass
+    
+    
+    def generate_epub(self):
+        """ TODO: Generate epub file """
+        pass
+    
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+from domain.models.Story import Story
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -10,6 +12,10 @@ async def root():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
+    
+    story = Story(prologue="Une histoire pour enfant qui se passe dans un chateau", plan="free").add_paragraph_step("Give suspens").add_paragraph_step("Give suspens").add_paragraph_step("Give suspens").add_paragraph_step("Prepare to the end").add_paragraph_step("Give conclusion").get_story()
+
+    print(story)
     
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000) # specify port as 8000
